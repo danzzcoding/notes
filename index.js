a38f28c4997162de2e8c536b5d330279c119c3ed
Danzz Coding <muhammadramdani196453@gmail.com>
2022-10-05 04:54:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-console.log(`NOTES: PINDAH REST API NGABB`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
-console.log(`https://danzzapi.xyz`)
+__path = process.cwd()
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+	res.sendFile(__path + '/index.html')
+	})
+
+app.listen(process.env.PORT || 8080, () => console.log('Server work at localhost:8080'))
